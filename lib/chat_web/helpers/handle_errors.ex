b5137dfbfd032d09b36e24ae4ea5282d30dda69e
Vinicius Batista <viniciusbfs2012@gmail.com>
2018-04-21 20:19:55 -0300

    helpers: add handle_errors

---
+++ lib/chat_web/helpers/handle_errors.ex
@@ -0,0 +1,20 @@
+defmodule ChatWeb.Helpers.HandleErrors do
+  def handle_errors(fun) do
+    fn source, args, info ->
+      case Absinthe.Resolution.call(fun, source, args, info) do
+        {:error, %Ecto.Changeset{} = changeset} -> format_changeset(changeset)
+        val -> val
+      end
+    end
+  end
+
+  def format_changeset(changeset) do
+    errors =
+      changeset.errors
+      |> Enum.map(fn({key, {value, _context}}) ->
+        [message: "#{key} #{value}"]
+      end)
+
+    {:error, errors}
+  end
+end
