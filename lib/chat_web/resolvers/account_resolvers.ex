6a2d1241d344b2132ced546722f7f31344794d29
Vinicius Batista <viniciusbfs2012@gmail.com>
2018-04-26 09:08:36 -0300

    accounts: refactor account resolver

--- lib/chat_web/resolvers/account_resolvers.ex
+++ lib/chat_web/resolvers/account_resolvers.ex
@@ -11,38 +11,40 @@ defmodule ChatWeb.Resolvers.AccountResolver do
   end
 
   def login_user(_, %{input: %{email: email, password: password}}, _) do
-    case Auth.find_user_and_check_password(email, password) do
-      {:ok, user} ->
-        user
-        |> Guardian.encode_and_sign(%{}, token_type: :bearer)
-        |> Auth.generate_refresh_token()
-
-      error ->
-        error
-    end
+    email
+    |> Auth.find_user_and_check_password(password)
+    |> generate_tokens
   end
+
+  defp generate_tokens({:ok, user}) do
+    user
+    |> Guardian.encode_and_sign(%{}, token_type: :bearer)
+    |> Auth.generate_refresh_token()
+  end
+
+  defp generate_tokens(error), do: error
 
   def new_access_token(_, %{refresh_token: refresh_token}, _) do
     refresh_token
     |> Auth.get_user_by_refresh_token()
-    |> case do
-      {:ok, user} ->
-        {:ok, access_token, %{"typ" => type}} =
-          user
-          |> Guardian.encode_and_sign(%{}, token_type: :bearer)
-
-        tokens =
-          %{}
-          |> Map.put(:refresh_token, refresh_token)
-          |> Map.put(:access_token, access_token)
-          |> Map.put(:type, type)
-
-        {:ok, tokens}
-
-      error ->
-        error
-    end
+    |> generate_new_access_token(refresh_token)
   end
+
+  defp generate_new_access_token({:ok, user}, refresh_token) do
+    {:ok, access_token, %{"typ" => type}} =
+      user
+      |> Guardian.encode_and_sign(%{}, token_type: :bearer)
+
+    tokens =
+      %{}
+      |> Map.put(:refresh_token, refresh_token)
+      |> Map.put(:access_token, access_token)
+      |> Map.put(:type, type)
+
+    {:ok, tokens}
+  end
+
+  defp generate_new_access_token(error, _), do: error
 
   def profile(_, _, %{context: %{current_user: current_user}}), do: {:ok, current_user}
 
