1ba4032030ab52406b68d0a77c771027a350c6e7
Vinicius Batista <viniciusbfs2012@gmail.com>
2018-04-27 09:18:50 -0300

    chore: update formatter

--- .formatter.exs
+++ .formatter.exs
@@ -1,4 +1,21 @@
 # Used by "mix format"
 [
-  inputs: ["mix.exs", "{config,lib,test}/**/*.{ex,exs}"]
+  inputs: [
+    "mix.exs",
+    "{config,lib,test,priv}/**/*.{ex,exs}"
+  ],
+  locals_without_parens: [
+    # Formatter tests
+    assert_format: 2,
+    assert_format: 3,
+    assert_same: 1,
+    assert_same: 2,
+
+    # Errors tests
+    assert_eval_raise: 3,
+
+    # Mix tests
+    in_fixture: 2,
+    in_tmp: 2
+  ]
 ]
