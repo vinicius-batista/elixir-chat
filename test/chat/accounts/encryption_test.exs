e945886c6234a323fa396dde0e6edcd62f9c4c7f
Vinicius Batista <viniciusbfs2012@gmail.com>
2018-04-27 10:28:47 -0300

    accounts: add encryption tests

---
+++ test/chat/accounts/encryption_test.exs
@@ -0,0 +1,21 @@
+defmodule Chat.EncryptionTest do
+  use Chat.DataCase
+  alias Chat.Accounts.Encryption
+
+  @password "test-password"
+
+  test "password_hashing return encrypted password" do
+    password_hash = Encryption.password_hashing(@password)
+    assert password_hash != @password
+  end
+
+  test "validate_password return true for equal password" do
+    password_hash = Encryption.password_hashing(@password)
+    assert Encryption.validate_password(@password, password_hash) == true
+  end
+
+  test "validate_password return false for wrong password" do
+    password_hash = Encryption.password_hashing(@password)
+    assert Encryption.validate_password("testpassword", password_hash) == false
+  end
+end
