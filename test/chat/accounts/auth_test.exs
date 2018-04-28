8071abed4fad9aba81ef31015fc459b72abcd8d8
Vinicius Batista <viniciusbfs2012@gmail.com>
2018-04-28 11:22:59 -0300

    accounts: finish auth tests

--- test/chat/accounts/auth_test.exs
+++ test/chat/accounts/auth_test.exs
@@ -12,7 +12,8 @@ defmodule Chat.AuthTest do
 
   setup do
     {:ok, user} = Accounts.create_user(@valid_attrs)
-    {:ok, %{user: %Accounts.User{user | password: nil}}}
+    {:ok, token} = Accounts.create_token(%{user_id: user.id})
+    {:ok, %{user: %Accounts.User{user | password: nil}, refresh_token: token.refresh_token}}
   end
 
   test "check_password returns true for correct password", %{user: user} do
@@ -28,7 +29,7 @@ defmodule Chat.AuthTest do
   end
 
   test "find_user_and_check_password returns user with correct email and pass", %{user: user} do
-    user_auth = Auth.find_user_and_check_password("email@email.com", "test")
+    {:ok, user_auth} = Auth.find_user_and_check_password("email@email.com", "test")
     assert user_auth == user
   end
 
@@ -40,5 +41,60 @@ defmodule Chat.AuthTest do
   test "find_user_and_check_password returns error with incorrect email" do
     assert Auth.find_user_and_check_password("emaiemail.com", "password") ==
              {:error, "Could not find user with email provided"}
+  end
+
+  test "generate_tokens returns all auth tokens", %{user: user} do
+    with {:ok, tokens} <- Auth.generate_tokens(user) do
+      assert is_map(tokens)
+      assert is_bitstring(tokens.type)
+      assert is_bitstring(tokens.access_token)
+      assert is_bitstring(tokens.refresh_token)
+    end
+  end
+
+  test "generate_tokens returns error" do
+    response = Auth.generate_tokens(%{})
+    assert response == {:error, "Unknow resource type"}
+  end
+
+  test "generate_new_access_token returns new auth tokens", %{
+    user: user,
+    refresh_token: refresh_token
+  } do
+    {:ok, tokens} = Auth.generate_new_access_token(user, refresh_token)
+    assert is_map(tokens)
+    assert is_bitstring(tokens.type)
+    assert is_bitstring(tokens.access_token)
+    assert is_bitstring(tokens.refresh_token)
+    assert tokens.refresh_token == refresh_token
+  end
+
+  test "get_user_by_refresh_token returns user", %{
+    user: user,
+    refresh_token: refresh_token
+  } do
+    {:ok, user_by_token} = Auth.get_user_by_refresh_token(refresh_token)
+    assert user == user_by_token
+  end
+
+  test "get_user_by_refresh_token returns error" do
+    assert Auth.get_user_by_refresh_token("test") ==
+             {:error, "Could not find user with refresh token provided"}
+  end
+
+  test "revoke_refresh_token return ok and token revoked", %{
+    user: user,
+    refresh_token: refresh_token
+  } do
+    {:ok, token} = Auth.revoke_refresh_token(refresh_token, user.id)
+    assert token.refresh_token == refresh_token
+    assert token.is_revoked
+  end
+
+  test "revoke_refresh_token return error could not find", %{
+    refresh_token: refresh_token
+  } do
+    {:error, reason} = Auth.revoke_refresh_token(refresh_token, 0)
+    assert "Could not find refresh token provided" == reason
   end
 end
