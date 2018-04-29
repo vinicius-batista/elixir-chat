fe906aba9435d316071faf492af865f73c22cebe
Vinicius Batista <viniciusbfs2012@gmail.com>
2018-04-28 22:14:46 -0300

    chore: remove absinthe_ecto depedencie

--- mix.exs
+++ mix.exs
@@ -42,7 +42,6 @@ defmodule Chat.Mixfile do
       {:absinthe, "~> 1.4", override: true},
       {:absinthe_phoenix, "~> 1.4"},
       {:absinthe_plug, "~> 1.4"},
-      {:absinthe_ecto, ">= 0.0.0"},
       {:bcrypt_elixir, "~> 1.0"},
       {:guardian, "~> 1.0"}
     ]
